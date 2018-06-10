ee50415bfedd53479d264809be85a8bb3c0a5e0d
VicRen <vic.ren@me.com>
2018-06-10 20:56:33 +0800

    添加官网 Sample

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,9 @@
+import 'package:HelloFlutter/animatedlist.dart';
+import 'package:HelloFlutter/appbarbasic.dart';
+import 'package:HelloFlutter/appbarbottomwidget.dart';
+import 'package:HelloFlutter/expansiontitle.dart';
 import 'package:HelloFlutter/flutterhome.dart';
+import 'package:HelloFlutter/imagepicker.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(new FlutterLearning());
@@ -6,6 +11,9 @@ void main() => runApp(new FlutterLearning());
 class FlutterLearning extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return new FlutterHome();
+    return new MediaQuery(
+        data: new MediaQueryData(),
+        child: new MaterialApp(home: new MyHomePage())
+    );
   }
 }
