514a4e3392ae2f98c8c19d7609eb1b9f65462797
VicRen <vic.ren@me.com>
2018-06-12 13:09:04 +0800

    添加 Navigator 练习

---
+++ lib/navigator/screens/second.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class Second extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+        appBar: new AppBar(
+          title: new Text('Second'),
+        ),
+        body: new Container(
+          padding: new EdgeInsets.all(32.0),
+          child: new Column(
+            children: <Widget>[
+              new Text('This is the Second Screen'),
+              new RaisedButton(child: new Text('Next'),
+                  onPressed: () {Navigator.of(context).pushNamed('/Third');})
+            ],
+          ),
+        )
+    );
+  }
+
+}
