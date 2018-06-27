915a58f4be31c076b3777918da66c0a2930237f5
VicRen <vic.ren@me.com>
2018-06-27 19:14:56 +0800

    尝试 dart testing

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -4,26 +4,19 @@
 // find child widgets in the widget tree, read text, and verify that the values of widget properties
 // are correct.
 
-import 'package:HelloFlutter/main.dart';
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
+import 'dart:convert' as json;
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(new FlutterLearning());
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+  List<int> parse(String jsonStr) {
+    final parse = json.jsonDecode(jsonStr);
+    final listOfString = List<int>.from(parse);
+    return listOfString;
+  }
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+  test('Tying Testing', () {
+    const jsonString = "[12345,56789]";
+    expect(parse(jsonString).first, 12345);
   });
 }
