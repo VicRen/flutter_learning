4fa194d6810afbf3bc5d93e42d5c07a22f2b08d8
VicRen <vic.ren@me.com>
2018-06-30 20:23:16 +0800

    添加帮助选项

--- lib/home.dart
+++ lib/home.dart
@@ -56,6 +56,13 @@ class _HomePageState extends State<HomePage> {
           },
         ),
         new ListTile(
+          leading: new Icon(Icons.help),
+          title: new Text('Help'),
+          onTap: () {
+            _popDrawer(DrawerAction.SETTINGS);
+          },
+        ),
+        new ListTile(
           leading: new Icon(Icons.info),
           title: new Text('About'),
           onTap: () {
